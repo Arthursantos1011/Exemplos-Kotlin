aa33ed3603660f79524dbf94a9e386f5ba96a6a4
Arthursantos1011 <arthur.santos210@etec.sp.gov.br>
2024-03-16 20:23:21 -0300

    Atividade kotlin

---
+++ Ativ2/src/Main.kt
@@ -0,0 +1,19 @@
+
+fun main(args: Array<String>) {
+
+
+    println("Digite o primeiro número: ")
+
+    val X: Int = readLine()!!.toInt()
+
+
+    println("Digite o segundo número: ")
+
+    val Y: Int = readLine()!!.toInt()
+
+
+    val resultado: Int = X*Y
+
+
+    println("O resultado da multiplicação é $resultado")
+}
